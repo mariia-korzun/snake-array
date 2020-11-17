9f66d47dab4b56da84399e8de511dd9fb7ccf324
Mariia Korzun <3151020m@gmail.com>
2020-11-18 00:05:34 +0200

    Snake array is completed.

--- src/snake-array.js
+++ src/snake-array.js
@@ -1,6 +1,28 @@
 const snakeArray = (snakeStart) => {
-    // Implementation here
-    return;
-}
+    let rows = 6
+    let columns = 7
+    let matrix = []
+    while (matrix.length < rows) {
+        matrix.push([])
+    }
+    let vectorsArray = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    let indexOfVector = 0
+    let vector = vectorsArray[indexOfVector]
+    let cx = 0
+    let cy = 0
 
+    while (matrix[cx][cy] === undefined) {
+        matrix[cx][cy] = snakeStart++
+        let cxNew = cx + vector[0]
+        let cyNew = cy + vector[1]
+        if (cxNew === rows || cyNew === columns || cxNew === -1 || cyNew === -1 || matrix[cxNew][cyNew] !== undefined ) {
+            indexOfVector = (indexOfVector + 1) % vectorsArray.length
+            vector = vectorsArray[indexOfVector]
+        }
+        cx = cx + vector[0]
+        cy = cy + vector[1]
+
+    }
+    return matrix;
+}
 module.exports = snakeArray;
